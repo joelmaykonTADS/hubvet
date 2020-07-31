4de4c2380a0aadc3f1c40e1e1384ec2cad4694df
joelmaykon <joelmaykon94@gmail.com>
2020-07-31 17:22:07 -0300

    tooltip central de ajuda

--- hubvet/src/components/Footer.vue
+++ hubvet/src/components/Footer.vue
@@ -4,8 +4,23 @@
       {{ new Date().getFullYear() }} — <v-icon>mdi-home</v-icon>
       <strong>Desenvolvedor: Joel Maykon</strong>
     </v-col>
-    <v-btn absolute dark fab buttom outlined right small color="grey">
-      <v-icon>mdi-help</v-icon>
-    </v-btn>
+    <v-tooltip v-model="show" bottom>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+          absolute
+          dark
+          fab
+          buttom
+          outlined
+          right
+          small
+          color="grey"
+        >
+          <v-icon>mdi-help</v-icon>
+        </v-btn>
+      </template></v-tooltip
+    >
   </v-footer>
 </template>
