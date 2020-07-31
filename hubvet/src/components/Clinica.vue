a22b763383b51d7a629707ef47ec2ef28d147ac6
joelmaykon <joelmaykon94@gmail.com>
2020-07-31 19:14:13 -0300

    componente para o ambiente cliníca

---
+++ hubvet/src/components/Clinica.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row class="text-left" align="center" justify="center">
+      <v-col cols="4">
+        <v-flex>
+          <v-img
+            :src="require('../assets/clinica.svg')"
+            class="mt-10"
+            contain
+            height="200"
+          />
+          <v-divider />
+        </v-flex>
+        <p class="subheading grey--text text--darken-1 font-weight-medium">
+          <b class="font-weight-bold">Gerencie,</b>
+          sua Clínica e ainda tenha acesso a centenas de Laboratórios
+          veterinários pelo Brasil. Em um ambiente 100% seguro e
+          <b class="font-weight-bold">escalável.</b>
+        </p>
+      </v-col>
+    </v-row>
+    <v-row class="text-left" align="center" justify="center">
+      <v-col cols="3">
+        <v-select
+          v-model="item"
+          color="grey"
+          :items="items"
+          label="O que você deseja fazer?"
+        ></v-select>
+        <v-btn block color="teal" dark>Realizar setup</v-btn>
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center">
+      <v-col cols="5">
+        <v-row justify="space-around">
+          <router-link to="/setup-laboratorio">
+            <v-btn class="ma-2" text tile large color="grey">
+              <v-icon left>fa fa-angle-left</v-icon>Voltar
+            </v-btn>
+          </router-link>
+          <v-radio-group v-model="radioGroup" row>
+            <v-radio color="teal" :value="1" disabled></v-radio>
+            <v-radio color="teal" :value="2" disabled></v-radio>
+            <v-radio color="teal" :value="3"></v-radio>
+            <v-radio color="teal" :value="4" disabled></v-radio>
+            <v-radio color="teal" :value="5" disabled></v-radio>
+          </v-radio-group>
+          <router-link to="/setup-clinica">
+            <v-btn class="ma-2" text tile large color="grey">
+              Próximo
+              <v-icon right>fa fa-angle-right</v-icon>
+            </v-btn>
+          </router-link>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Usuario",
+  data: () => ({
+    radioGroup: 3,
+    item: "Cuidar da minha clínica",
+    items: ["Cuidar da minha clínica"],
+  }),
+};
+</script>
--- hubvet/src/views/Setup.vue
+++ hubvet/src/views/Setup.vue
@@ -2,6 +2,7 @@
   <div class="setup">
     <Laboratorio v-show="laboratorio" />
     <Usuario v-show="usuario" />
+    <Clinica v-show="clinica" />
     <Footer />
   </div>
 </template>
@@ -10,19 +11,21 @@
 // @ is an alias to /src
 import Laboratorio from "@/components/Laboratorio.vue";
 import Usuario from "@/components/Usuario.vue";
+import Clinica from "@/components/Clinica.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Setup",
-  props:{
-    laboratorio:Boolean,
-    usuario:Boolean,
-    clinica:Boolean
+  props: {
+    laboratorio: Boolean,
+    usuario: Boolean,
+    clinica: Boolean,
   },
   components: {
     Laboratorio,
     Usuario,
-    Footer
-  }
+    Clinica,
+    Footer,
+  },
 };
 </script>
