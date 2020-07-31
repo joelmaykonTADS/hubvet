31b255eb3bba5243643fbec99486b6cdecd83652
joelmaykon <joelmaykon94@gmail.com>
2020-07-31 16:36:07 -0300

    componente setup

---
+++ hubvet/src/views/Setup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="setup">
+    <Ambiente />
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Ambiente from "@/components/Ambiente.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Setup",
+  components: {
+    Ambiente,
+    Footer
+  }
+};
+</script>
