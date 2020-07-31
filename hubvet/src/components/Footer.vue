1733025004979616fadf4f1555c6dcb398b5f1af
joelmaykon <joelmaykon94@gmail.com>
2020-07-31 17:16:21 -0300

    botão de ajuda no footer

--- hubvet/src/components/Footer.vue
+++ hubvet/src/components/Footer.vue
@@ -1,10 +1,11 @@
 <template>
   <v-footer absolute>
-    <v-col
-      class="text-center"
-      cols="12"
-    >
-      {{ new Date().getFullYear() }} —  <v-icon>mdi-home</v-icon> <strong>Desenvolvedor: Joel Maykon</strong>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} — <v-icon>mdi-home</v-icon>
+      <strong>Desenvolvedor: Joel Maykon</strong>
     </v-col>
+    <v-btn absolute dark fab buttom outlined right small color="grey">
+      <v-icon>mdi-help</v-icon>
+    </v-btn>
   </v-footer>
 </template>
