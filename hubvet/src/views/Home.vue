56e8a0194a9591f6c688ad6304acd24bf0c35665
joelmaykon <joelmaykon94@gmail.com>
2020-07-31 16:35:29 -0300

    adicionado o footer ao componente Home

--- hubvet/src/views/Home.vue
+++ hubvet/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <BemVindo />
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import BemVindo from "@/components/BemVindo.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Home",
   components: {
-    BemVindo
+    BemVindo,
+    Footer
   }
 };
 </script>
