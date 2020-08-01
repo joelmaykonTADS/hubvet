fa6494d62558f62d830d0cc9fa812651a0061427
joelmaykon <joelmaykon94@gmail.com>
2020-08-01 20:57:28 -0300

    passando itens via props para a lista de itens

--- hubvet/src/components/ListaItensGrupos.vue
+++ hubvet/src/components/ListaItensGrupos.vue
@@ -150,97 +150,11 @@ export default {
       }));
     }
   },
+  props:{
+    itens:Array
+  },
   data: () => ({
-    novosItems: [],
-    itens: [
-      {
-        id: 1,
-        sigla: "GLIC",
-        nome: "Item 1",
-        prazo: 0,
-        valor: 25.76,
-        descricao: "",
-        itens: []
-      },
-      {
-        id: 2,
-        sigla: "GLIC 2",
-        nome: "Item 2",
-        prazo: 0,
-        valor: 25.76,
-        descricao:
-          "Aqui irão conter mais informações sobre o item (Ex: Descrição )",
-        itens: []
-      },
-      {
-        id: 3,
-        sigla: "GP 1",
-        nome: "Grupo 1",
-        prazo: 0,
-        valor: 19.68,
-        descricao: "",
-        itens: []
-      },
-      {
-        id: 4,
-        sigla: "GP 2",
-        nome: "Grupo 2",
-        prazo: 2,
-        valor: 60.03,
-        descricao: "",
-        itens: [
-          {
-            id: 1,
-            sigla: "IT7",
-            nome: "Item 7",
-            prazo: 2,
-            valor: 18.07
-          },
-          {
-            id: 2,
-            sigla: "IT3",
-            nome: "Item 3",
-            prazo: 1,
-            valor: 18.07
-          },
-
-          {
-            id: 3,
-            sigla: "IT6",
-            nome: "Item 6",
-            prazo: 1,
-            valor: 23.89
-          }
-        ]
-      },
-      {
-        id: 5,
-        sigla: "GP 3",
-        nome: "Grupo 3",
-        prazo: 0,
-        valor: 25.76,
-        descricao: "",
-        itens: []
-      },
-      {
-        id: 6,
-        sigla: "IT 7",
-        nome: "Item 7",
-        prazo: 0,
-        valor: 25.76,
-        descricao: "",
-        itens: []
-      },
-      {
-        id: 7,
-        sigla: "IT20",
-        nome: "Item 20",
-        prazo: 0,
-        valor: 25.76,
-        descricao: "",
-        itens: []
-      }
-    ]
+    novosItems: []
   })
 };
 </script>
--- hubvet/src/views/WorkSpace.vue
+++ hubvet/src/views/WorkSpace.vue
@@ -34,7 +34,7 @@
         </v-row>
         <v-row>
           <v-col cols="8">
-            <ListaItensGrupos />
+            <ListaItensGrupos :itens="itens"/>
           </v-col>
           <v-col cols="4">
             <Carrinho />
@@ -66,7 +66,96 @@ export default {
   data: () => ({
     drawer: true,
     filtroItens: true,
-    filtroGrupos: false
+    filtroGrupos: false,
+    itens: [
+      {
+        id: 1,
+        sigla: "GLIC",
+        nome: "Item 1",
+        prazo: 0,
+        valor: 25.76,
+        descricao: "",
+        itens: []
+      },
+      {
+        id: 2,
+        sigla: "GLIC 2",
+        nome: "Item 2",
+        prazo: 0,
+        valor: 25.76,
+        descricao:
+          "Aqui irão conter mais informações sobre o item (Ex: Descrição )",
+        itens: []
+      },
+      {
+        id: 3,
+        sigla: "GP 1",
+        nome: "Grupo 1",
+        prazo: 0,
+        valor: 19.68,
+        descricao: "",
+        itens: []
+      },
+      {
+        id: 4,
+        sigla: "GP 2",
+        nome: "Grupo 2",
+        prazo: 2,
+        valor: 60.03,
+        descricao: "",
+        itens: [
+          {
+            id: 1,
+            sigla: "IT7",
+            nome: "Item 7",
+            prazo: 2,
+            valor: 18.07
+          },
+          {
+            id: 2,
+            sigla: "IT3",
+            nome: "Item 3",
+            prazo: 1,
+            valor: 18.07
+          },
+
+          {
+            id: 3,
+            sigla: "IT6",
+            nome: "Item 6",
+            prazo: 1,
+            valor: 23.89
+          }
+        ]
+      },
+      {
+        id: 5,
+        sigla: "GP 3",
+        nome: "Grupo 3",
+        prazo: 0,
+        valor: 25.76,
+        descricao: "",
+        itens: []
+      },
+      {
+        id: 6,
+        sigla: "IT 7",
+        nome: "Item 7",
+        prazo: 0,
+        valor: 25.76,
+        descricao: "",
+        itens: []
+      },
+      {
+        id: 7,
+        sigla: "IT20",
+        nome: "Item 20",
+        prazo: 0,
+        valor: 25.76,
+        descricao: "",
+        itens: []
+      }
+    ]
   })
 };
 </script>
