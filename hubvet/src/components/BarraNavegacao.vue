e7c3ea4020b18c4e768b4726076803e0a8448143
joelmaykon <joelmaykon94@gmail.com>
2020-08-02 17:22:48 -0300

    icones laterais

--- hubvet/src/components/BarraNavegacao.vue
+++ hubvet/src/components/BarraNavegacao.vue
@@ -1,17 +1,89 @@
 <template>
-  <v-navigation-drawer mini-variant v-model="drawer" clipped app color="white">
+  <v-navigation-drawer mini-variant  mini-variant-width="100" v-model="drawer" clipped app color="white">
     <v-list dense>
       <v-list-item link>
         <v-list-item-action>
-          <v-icon>mdi-home</v-icon>
+          <v-icon>fas fa-home</v-icon>
         </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-title>Home</v-list-item-title>
+          <v-list-item-title>fas fa-home</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
       <v-list-item link>
         <v-list-item-action>
-          <v-icon>mdi-email</v-icon>
+          <v-icon>fas fa-envelope</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider />
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>fas fa-adjust</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon color="teal" outlined>fas fa-file-alt</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>fas fa-ambulance</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>fas fa-file-medical-alt</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>fas fa-briefcase-medical</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>fas fa-cog</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>fas fa-question-circle</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>fas fa-ellipsis-h</v-icon>
         </v-list-item-action>
         <v-list-item-content>
           <v-list-item-title>Contact</v-list-item-title>
